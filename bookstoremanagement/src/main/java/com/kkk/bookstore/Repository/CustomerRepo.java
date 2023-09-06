44b9c6b21b14f51375d0a3f8a07c87b058389d08
KiranKumarSingh18 <kirankumarsingh789@gmail.com>
2023-09-06 19:22:37 +0530

    Update CustomerRepo.java

--- bookstoremanagement/src/main/java/com/kkk/bookstore/Repository/CustomerRepo.java
+++ bookstoremanagement/src/main/java/com/kkk/bookstore/Repository/CustomerRepo.java
@@ -5,5 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.kkk.bookstore.entity.Customer;
 
 public interface CustomerRepo extends JpaRepository<Customer, Integer>{
+@Query("SELECT CASE WHEN COUNT(c) = 1 THEN true ELSE false END FROM Customer c WHERE c.email = :emailId AND c.password = :password")
+	    boolean validateLogin(
+	        @Param("emailId") String email,
+	        @Param("password") String password
+	    );
+	
 
 }
